9a16014047deaa8b859203c154ba95bb2a3ffec8
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-12 20:53:38 -0600

    [DB] Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Client::Client.create(name: 'Sony')
+Client::Schedule.create(start: '8', end: '20', day_of_week: 'monday', client_clients_id: 1)
+Worker::Worker.create(name: 'John', client: Client::Client.first)
+Worker::Schedule.create(time: '2022-08-08 10:00:00', worker: Worker::Worker.first)

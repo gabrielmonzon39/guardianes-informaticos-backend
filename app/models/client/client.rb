9c26563b8a05e5511d388611522e2f2cf5b15ff7
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-18 15:00:04 -0600

    [Client][Model] Worker relations added

--- app/models/client/client.rb
+++ app/models/client/client.rb
@@ -1,4 +1,5 @@
 class Client::Client < ApplicationRecord
   validates :name, presence: true
+  has_many :workers, class_name: 'Worker::Worker', foreign_key: 'client_clients_id'
   has_many :schedules, class_name: 'Client::Schedule', dependent: :destroy, foreign_key: 'client_clients_id'
 end

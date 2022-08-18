e691f89fd89a1089e1bbf52ad7538d83fadb829f
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-18 14:56:56 -0600

    [DB][Worker] Added color to worker migration

--- db/migrate/20220811231122_create_worker_workers.rb
+++ db/migrate/20220811231122_create_worker_workers.rb
@@ -2,6 +2,7 @@ class CreateWorkerWorkers < ActiveRecord::Migration[7.0]
   def change
     create_table :worker_workers do |t|
       t.string :name
+      t.string :color
       t.references :client_clients, null: false, foreign_key: true
 
       t.timestamps

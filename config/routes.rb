93dea97409bbac1bdcbf636fc68211b46ee154a9
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-18 14:59:23 -0600

    [Routes] Routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post '/confirm_schedules', to: 'worker#confirm_schedules'
+
+  get '/clients', to: 'client#index'
+  get '/clients/:id/weeks', to: 'client#weeks'
+  get '/clients/:id/weeks/:monday_date', to: 'client#schedule'
+  get '/clients/:id/weeks/:monday_date/edit', to: 'client#schedule_edit'
+
+  post '/workers/toggle-schedule', to: 'worker#toggle_schedule'
 end

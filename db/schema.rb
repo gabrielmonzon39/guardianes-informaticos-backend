024caa7dfd8fe5fe3501dabfcf1c37aeaccc8be5
Gabriel Monzon <gabriel.monzon39@gmail.com>
2022-08-19 02:30:01 -0600

    [DB] Schema

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_020717) do
 
   create_table "worker_workers", force: :cascade do |t|
     t.string "name"
+    t.string "color"
     t.bigint "client_clients_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
